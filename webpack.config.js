909f195438f65b13942e9716801de451e6b3eec0
Adam Edgett <AdamEdgett@gmail.com>
2017-04-25 20:55:41 -0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractLess = new ExtractTextPlugin({
+  filename: '[name].css',
+});
+
+module.exports = {
+  context: __dirname,
+
+  entry: {
+    sender: [
+      'babel-polyfill',
+      './src/js/sender.js',
+      './src/less/sender.less',
+    ],
+    receiver: [
+      'babel-polyfill',
+      './src/js/receiver.js',
+      './src/less/receiver.less',
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [
+          path.resolve(__dirname, 'src/js'),
+        ],
+        exclude: [
+          /node_modules/,
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react'],
+        },
+      },
+
+      {
+        test: /\.less$/,
+        include: [
+          path.resolve(__dirname, 'src/less'),
+        ],
+        use: extractLess.extract({
+          use: [
+            'css-loader',
+            'less-loader',
+          ],
+          fallback: 'style-loader',
+        }),
+      },
+    ],
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, './src/js/sender'),
+      path.resolve(__dirname, './src/less'),
+    ],
+    extensions: ['.js', '.jsx', '.less', '.json'],
+  },
+  plugins: [
+    extractLess,
+  ],
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
+  devServer: {
+    contentBase: './public',
+    port: '8080',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    inline: true,
+  },
+  stats: 'verbose',
+};
